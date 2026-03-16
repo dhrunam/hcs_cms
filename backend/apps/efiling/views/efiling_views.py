db72aa0190c046ad44fbd76b12f36c14f966b15d
sagardev1995 <sagaradh.nic@gmail.com>
2026-03-16 15:57:57 +0530

    Completing initial inputs and litigant form

--- backend/apps/efiling/views/efiling_views.py
+++ backend/apps/efiling/views/efiling_views.py
@@ -1,5 +1,6 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-
+from rest_framework.response import Response
+from rest_framework import status
 from apps.core.models import Efiling
 from apps.efiling.serializers.efiling_serializers import EfilingSerializer
  
@@ -8,6 +9,20 @@ from apps.efiling.serializers.efiling_serializers import EfilingSerializer
 class EfilingListCreateView(ListCreateAPIView):
     queryset = Efiling.objects.filter(is_active=True).order_by('-id')
     serializer_class = EfilingSerializer
+
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+
+        return Response(
+            {
+                "id": instance.id,
+                "e_filing_number": instance.e_filing_number
+            },
+            status=status.HTTP_201_CREATED
+        )
     
 
 
