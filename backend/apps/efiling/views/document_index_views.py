4423547f94cc734928da4d862c9f46e49f9a0f11
Jwala Lama <jwalalama@outlook.com>
2026-03-16 14:15:39 +0530

    added is_active filter in Get operations

--- backend/apps/efiling/views/document_index_views.py
+++ backend/apps/efiling/views/document_index_views.py
@@ -3,9 +3,9 @@ from apps.core.models import DocumentIndex
 from apps.efiling.serializers.document_index_serializers import DocumentIndexSerializer
 
 class DocumentIndexListCreateView(generics.ListCreateAPIView):
-    queryset = DocumentIndex.objects.all()
+    queryset = DocumentIndex.objects.filter(is_active=True).order_by('-id')
     serializer_class = DocumentIndexSerializer
 
 class DocumentIndexRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = DocumentIndex.objects.all()
+    queryset = DocumentIndex.objects.filter(is_active=True).order_by('-id')
     serializer_class = DocumentIndexSerializer
