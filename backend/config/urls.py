c560e3bc505cfd9c824d72b4336f453dd8436936
sagardev1995 <sagaradh.nic@gmail.com>
2026-03-17 17:23:58 +0530

    Efiling Form completion

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('apps.accounts.urls', namespace='accounts')),
-    path('api/v1/cases/', include('apps.cases.urls', namespace='cases')),
+    # path('api/v1/cases/', include('apps.cases.urls', namespace='cases')),
     path('api/v1/cis/', include('apps.cis.urls', namespace='cis')),
     path('api/v1/efiling/', include('apps.efiling.urls', namespace='efiling')),
     path('api/v1/master/', include('apps.master.urls', namespace='master')),
---
+++ frontend/src/app/areas/advocate/dashboard/efiling/new-filing/upload-documents/upload-documents.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UploadDocuments } from './upload-documents';
+
+describe('UploadDocuments', () => {
+  let component: UploadDocuments;
+  let fixture: ComponentFixture<UploadDocuments>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UploadDocuments],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UploadDocuments);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
