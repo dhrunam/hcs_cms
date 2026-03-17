447a68d3b406a0bd1cbe0f53a541217faf898043
Jwala Lama <jwalalama@outlook.com>
2026-03-17 15:02:18 +0530

    modified efiling documents index views

--- backend/apps/efiling/views/efiling_documents_index_views.py
+++ backend/apps/efiling/views/efiling_documents_index_views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from django.db import transaction
 
-from apps.core.models import EfilingDocumentsIndex
+from apps.core.models import EfilingDocumentsIndex, EfilingDocumentsScrutinyHistory
 from apps.efiling.serializers.efiling_document_index import (
     EfilingDocumentsIndexSerializer,
 )
@@ -13,9 +14,16 @@ class EfilingDocumentsIndexListCreateView(ListCreateAPIView):
         qs = EfilingDocumentsIndex.objects.all().order_by('-id')
         is_active = self.request.query_params.get('is_active')
         if is_active is not None:
-            # treat "true"/"1" as True
             qs = qs.filter(is_active=is_active.lower() in ['true', '1'])
         return qs
+
+    def perform_create(self, serializer):
+        with transaction.atomic():
+            instance = serializer.save()
+            EfilingDocumentsScrutinyHistory.objects.create(
+                efiling_document_index=instance,
+                recieved_at=instance.created_at,
+            )
 
 
 class EfilingDocumentsIndexRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
