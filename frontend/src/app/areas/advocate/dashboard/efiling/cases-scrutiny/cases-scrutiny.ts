849e7fcbc553de5d541b4d37f6c91d0fc02666dc
sagardev1995 <sagaradh.nic@gmail.com>
2026-03-19 12:08:19 +0530

    working on efiling scrutiny

---
+++ frontend/src/app/areas/advocate/dashboard/efiling/cases-scrutiny/cases-scrutiny.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-cases-scrutiny',
+  imports: [RouterOutlet],
+  templateUrl: './cases-scrutiny.html',
+  styleUrl: './cases-scrutiny.css',
+})
+export class CasesScrutiny {}
