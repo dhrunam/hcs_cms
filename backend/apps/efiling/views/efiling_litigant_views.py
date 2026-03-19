86b17c019f25d3e8e46ad70110997c7c5b30ee75
Jwala Lama <jwalalama@outlook.com>
2026-03-19 14:06:25 +0530

    filter by efiling_id

--- backend/apps/efiling/views/efiling_litigant_views.py
+++ backend/apps/efiling/views/efiling_litigant_views.py
@@ -12,6 +12,9 @@ class EfilingLitigantListCreateView(ListCreateAPIView):
     def get_queryset(self):
         qs = EfilingLitigant.objects.all().order_by('-id')
         is_active = self.request.query_params.get('is_active')
+        efiling_id = self.request.query_params.get('efiling_id')
+        if efiling_id is not None:
+            qs = qs.filter(efiling=efiling_id)
         if is_active is not None:
             # treat "true"/"1" as True
             qs = qs.filter(is_active=is_active.lower() in ['true', '1'])
