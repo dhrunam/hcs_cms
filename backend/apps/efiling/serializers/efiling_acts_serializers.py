21807152e35196f3a8a2f1e312d629b2e8c3a0b8
Jwala Lama <jwalalama@outlook.com>
2026-03-20 12:42:55 +0530

    act serializer changed

--- backend/apps/efiling/serializers/efiling_acts_serializers.py
+++ backend/apps/efiling/serializers/efiling_acts_serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from apps.core.models import EfilingActs
+from apps.core.models import ActT, EfilingActs
 
+
+class ActTSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ActT
+        fields = ['actname', 'actcode']
 
 class EfilingActsSerializer(serializers.ModelSerializer):
+    act = serializers.PrimaryKeyRelatedField(
+        queryset=ActT.objects.all(),
+        required=False,
+        allow_null=True,
+    )
+
     class Meta:
         model = EfilingActs
         fields = [
@@ -21,3 +32,13 @@ class EfilingActsSerializer(serializers.ModelSerializer):
             'updated_by',
         ]
         read_only_fields = ['id', 'created_at', 'updated_at']
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['act'] = (
+            ActTSerializer(instance.act).data
+            if instance.act_id
+            else None
+        )
+        return data
+
