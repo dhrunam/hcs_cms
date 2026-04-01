4a0917d40c0f139a6168c1b3156fae6c1f3f0765
sagardev1995 <sagaradh.nic@gmail.com>
2026-04-01 14:08:16 +0530

    ui fixes

---
+++ frontend/src/app/areas/reader/common/navbar/navbar.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Navbar } from './navbar';
+import { AuthService } from '../../../../auth.service';
+
+describe('Navbar', () => {
+  let component: Navbar;
+  let fixture: ComponentFixture<Navbar>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Navbar],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            initializeAuth: () => Promise.resolve(),
+            isLoggedIn: () => false,
+            login: () => {},
+            logout: () =>
+              Promise.resolve({
+                apiSessionLoggedOut: true,
+                ssoSessionLoggedOut: true,
+                tokensCleared: true,
+                success: true,
+              }),
+          },
+        },
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Navbar);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
