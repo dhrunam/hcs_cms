468aaf74f4fe844f5c89f459c04288c982676329
Raja Siddharth Raju <rsrajuofficial@gmail.com>
2026-04-02 12:34:08 +0530

    Checkin Code

--- backend/apps/efiling/views/notification_views.py
+++ backend/apps/efiling/views/notification_views.py
@@ -1,12 +1,30 @@
+import logging
+
+from django.db.utils import ProgrammingError
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
 
 from apps.efiling.models import EfilingNotification
 from apps.efiling.serializers.notification_serializers import EfilingNotificationSerializer
+
+logger = logging.getLogger(__name__)
 
 
 class EfilingNotificationListView(ListAPIView):
     serializer_class = EfilingNotificationSerializer
     pagination_class = None
+
+    def list(self, request, *args, **kwargs):
+        try:
+            return super().list(request, *args, **kwargs)
+        except ProgrammingError as exc:
+            if "efiling_notification" not in str(exc):
+                raise
+            logger.warning(
+                "efiling_notification missing; run: python manage.py migrate efiling",
+                exc_info=True,
+            )
+            return Response([])
 
     def get_queryset(self):
         role = self.request.query_params.get("role")
