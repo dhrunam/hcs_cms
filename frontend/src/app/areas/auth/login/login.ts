8306ffc50ec5518cbece2a0d6412485c10b1061f
Raja Siddharth Raju <rsrajuofficial@gmail.com>
2026-04-07 10:12:08 +0530

    Pushing code on 07.04.2026

--- frontend/src/app/areas/auth/login/login.ts
+++ frontend/src/app/areas/auth/login/login.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AuthService } from '../../../auth.service';
 
 @Component({
@@ -17,7 +17,30 @@ export class Login {
   constructor(
     private authService: AuthService,
     private route: ActivatedRoute,
-  ) {}
+    private router: Router,
+  ) {
+    const role = window.sessionStorage.getItem('user_group');
+    switch (role) {
+      case 'API_ADVOCATE':
+        this.router.navigate(['/advocate/dashboard/home']);
+        break;
+      case 'API_SCRUTINY_OFFICER':
+        this.router.navigate(['/scrutiny-officers/dashboard/home']);
+        break;
+      case 'API_COURT_READER':
+        this.router.navigate(['/reader/dashboard/home']);
+        break;
+      case 'API_LISTING_OFFICER':
+        this.router.navigate(['/listing-officers/dashboard/home']);
+        break;
+      case 'API_JUDGE':
+        this.router.navigate(['/judge/dashboard/home']);
+        break;
+      case 'API_STENOGRAPHER':
+        this.router.navigate(['/steno/dashboard/home']);
+        break;
+    }
+  }
 
   ngOnInit(): void {
     const authError = this.route.snapshot.queryParamMap.get('auth_error');
