a5189e38982e8b24c596829e17102f309f77cc95
Jwala Lama <jwalalama@outlook.com>
2026-04-08 17:21:08 +0530

    added new field filed_by in efiling_document

--- backend/apps/efiling/serializers/efiling_documents_serializers.py
+++ backend/apps/efiling/serializers/efiling_documents_serializers.py
@@ -32,6 +32,7 @@ class EfilingDocumentsSerializer(serializers.ModelSerializer):
             "updated_at",
             "created_by",
             "updated_by",
+            "filed_by",
         ]
         read_only_fields = ["id", "created_at", "updated_at"]
 
